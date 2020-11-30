d66b8395d451dd85fdcf09518698cc5c1aa9abc5
Eric Delaire <S058601@siege.axa-fr.intraxa>
2020-11-30 19:45:39 +0100

    Arret de la generation systematique de la table et amelioration de la generation des log

---
+++ create.sql
@@ -0,0 +1,1 @@
+create table patient (id bigint not null auto_increment, address varchar(200), birthdate datetime, firstname varchar(100), lastname varchar(100), phone varchar(12), sex varchar(1), primary key (id)) engine=InnoDB
